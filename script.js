6d0cad78159f8aca9bf93c18fa323d4af6787326
Will Vorngsam <will.vorngsam@rti1.com>
2024-04-06 02:14:46 -0500

    turn phone horizontal prompt

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // function onLoad() {
 //   document.getElementById("body").classList.add("show")
 // }
+const isMobile = navigator.userAgentData.mobile; //resolves true/false
 
 
 var img = new Image();
-img.onload = function() {
+img.onload = function () {
   document.getElementById("body").style.backgroundImage = img.src;
   document.getElementById("body").classList.add("show");
 }
@@ -21,37 +22,47 @@ const loadImage = src =>
     img.src = src;
   });
 
-loadImage("big_kimchi.jpg").then(image => 
-  {
-    console.log(image, `\nloaded? ${image.complete}`);
-    document.getElementById("cover").style.backgroundImage = `url(${image.src})` ;
-  }
-                                 
+loadImage("big_kimchi.jpg").then(image => {
+  console.log(image, `\nloaded? ${image.complete}`);
+  document.getElementById("cover").style.backgroundImage = `url(${image.src})`;
+}
+
 );
 
 
 
 
 
-document.body.addEventListener('click', nextImage, true); 
+document.body.addEventListener('click', nextImage, true);
 
-var index = 0
+var index = -1
 
-function nextImage()
-{
+function nextImage() {
   let newIndex = getRandomInt(13);
   //ensure we don't have the same one
-  console.log(newIndex,index)
-  while(newIndex == index)
-  {
+  console.log(newIndex, index)
+  while (newIndex == index) {
     newIndex = getRandomInt(13);
   }
   index = newIndex
-  document.getElementById("cover").style.backgroundImage = `url(/gallery/${index}.jpg)` ;
-  document.getElementById("cover").style.backgroundPosition = `50% 50%` ;
+  document.getElementById("cover").style.backgroundImage = `url(/gallery/${index}.jpg)`;
+  document.getElementById("cover").style.backgroundPosition = `50% 50%`;
   document.getElementsByClassName("title")[0].style.opacity = 0;
+
+  if (isMobile && (index == 0 || index == 12)) {
+    document.getElementsByClassName("title")[0].style.opacity = 1;
+    document.getElementsByClassName("title")[0].innerHTML = "Turn phone horizontal"
+  }
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+
+$(window).resize(function () {
+  //resize just happened, pixels changed
+  if (index >= 0) {
+    document.getElementsByClassName("title")[0].style.opacity = 0;
+  }
+});
